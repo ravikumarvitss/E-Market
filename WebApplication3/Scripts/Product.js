f40ed390fa004f35fb0d6cba6f2f9009f38d7bff
ravikumarvitss <ravikumar.vitss@gmail.com>
2017-06-04 21:57:15 +0530

    initial

---
+++ WebApplication3/Scripts/Product.js
@@ -0,0 +1,10 @@
+﻿var myProd = angular.module("myModule", [])
+               .controller("Mycontroller", function ($scope)
+               {
+                   var ProductDetails = [
+                       { ProductID: "1", ProdImage: "~/Content/vegetables.jpg", ProdDesc: "Fresh vegetables endowed with almost all of the nutritional principles that our body requires" },
+                       { ProductID: "2", ProdImage: "~/Content/cans-beverages-19492376.jpg", ProdDesc: "Fresh vegetables endowed with almost all of the nutritional principles that our body requires" }
+                   ];
+                   $scope.Product = ProductDetails;
+               });
+
---
+++ WebApplication3/Scripts/myApp.js
@@ -0,0 +1,28 @@
+﻿var myApp = angular.module("myApp", []);
+
+myApp.controller("mycontroller", function ($scope, $http)
+{  
+    
+    $scope.submit = function () {
+        if ($scope.username && $scope.password) {
+
+            $http({
+                method: "GET",
+                url: "http://localhost:56290/api/home"
+                
+            }).then(function mySuccess(response) {
+                $scope.myWelcome = response.data;
+                console.log($scope.myWelcome);
+            }, function myError(response) {
+                $scope.myWelcome = response.statusText;
+                console.log($scope.myWelcome);
+            });
+        } else {
+
+            alert("Invalid Login");
+        }
+    }
+   
+});
+
+
